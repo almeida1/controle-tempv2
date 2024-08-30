2d47768d0bbd9d7cc1beda4ea7de14a4ffffeb99
almeida1 <engsoftware1@gmail.com>
2024-08-29 22:30:38 -0300

    Refatoracao dos casos de teste  On branch master  Changes to be committed: 	deleted:    src/ObterEndereco.test1.js 	new file:   src/REQ01IncrementarTemperatura.test.js 	deleted:    src/REQ01IncremetarTemperatura.test.js 	deleted:    src/componentes/ConsultaCep/index.js 	deleted:    src/componentes/ConsultaCep/styles.css

---
+++ src/REQ01IncrementarTemperatura.test.js
@@ -0,0 +1,45 @@
+import "@testing-library/jest-dom";
+import {
+  render,
+  fireEvent,
+  screen,
+  queryAllByText,
+} from "@testing-library/react";
+import Adiciona from "./componentes/Adiciona";
+
+test("CT01- verifica o estado do contador na inicializacao", () => {
+  //dado que o contador foi inicializado
+  render(<Adiciona />);
+  //quando consulto o valor do contador
+  const valorContador = Number(screen.getByTestId("contador").textContent);
+  //entao contador = 0
+  expect(valorContador).toBe(0);
+});
+test("CT02- o valor do contador deve incrementar de 1 no click do botao", () => {
+  //dado que o contador foi inicializado
+  render(<Adiciona />);
+  //quando clico no botao de incremento
+  const btnIncrementa = screen.getByRole("button", { name: "Incremento" });
+  fireEvent.click(btnIncrementa);
+  //entao o valor do contador deve ser 1
+  const valorContador = Number(screen.getByTestId("contador").textContent);
+  expect(valorContador).toEqual(1);
+});
+test("CT03- o valor do contador deve decrementar de 1 no click do botao", () => {
+  //dado que o contador foi inicializado
+  render(<Adiciona />);
+  //quando clico no botao de decremento
+  const btnDecrementa = screen.getByRole("button", { name: "Decremento" });
+  fireEvent.click(btnDecrementa);
+  //entao o valor do contador deve ser -1
+  const valorContador = Number(screen.getByTestId("contador").textContent);
+  expect(valorContador).toEqual(-1);
+});
+test("CT04 - o botao reset deve registrar zero quando pressionado", () => {
+  render(<Adiciona />);
+  const btnReset = screen.getByText("Reset");
+  fireEvent.click(btnReset);
+  const valorContador = Number(screen.getByTestId("contador").textContent);
+  expect(btnReset).toBeTruthy();
+  expect(valorContador).toEqual(0);
+});
